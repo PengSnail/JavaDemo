b1a0cca56091fa6837cf189ce3b764edfd191656
PengG <2225625042@qq.com>
2020-10-19 21:07:53 +0800

    不可重入锁

---
+++ src/main/java/com/study/test/juc/aqs/lock/Test.java
@@ -0,0 +1,33 @@
+package com.study.test.juc.aqs.lock;
+
+/**
+ * @author PengG
+ * @date 2020/10/17 15:11
+ */
+public class Test {
+    public static void main(String[] args) {
+        MyLock lock = new MyLock();
+        new Thread(() -> {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " lock......");
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(Thread.currentThread().getName() + " unlock......");
+                lock.unlock();
+            }
+        },"t1").start();
+
+        new Thread(() -> {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " lock......");
+            try {
+            } finally {
+                System.out.println(Thread.currentThread().getName() + " unlock......");
+                lock.unlock();
+            }
+        },"t2").start();
+    }
+}
