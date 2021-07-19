51a23edb2bccba4ed4ec3f13266b299d734a6ec6
zhuangzp <17875511937@163.com>
2021-07-19 21:12:15 +0800

    Git学习

---
+++ src/main/java/com/study/algorithm/OnceNumber.java
@@ -0,0 +1,37 @@
+package com.study.algorithm;
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 示例1：
+ *      输入: [2,2,1]
+ *      输出: 1
+ * 示例2：
+ *      输入: [4,1,2,1,2]
+ *      输出: 4
+ * @author PengG
+ */
+public class OnceNumber {
+
+    static int count = 0;
+
+    static final Object O = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+
+    public static int singleNumber(int[] nums) {
+
+        return 0;
+    }
+}
